df7ae07771f75db6a7b52e964f356f0c999e30f0
aksje-app <dag.borch@tanabygg.no>
2026-05-01 20:52:57 +0200

    Delete check_pushover.py

--- check_pushover.py
+++
@@ -1,7 +0,0 @@
-
-from notifier import pushover_enabled, send_pushover_alert
-
-print("pushover_enabled:", pushover_enabled())
-ok, err = send_pushover_alert("Test fra Render Shell / check_pushover.py", title="Pushover test")
-print("sent:", ok)
-print("error:", err)
