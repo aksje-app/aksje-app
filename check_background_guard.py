a00f9b7dcaff03d17b69d637092593f1b22d9738
aksje-app <dag.borch@tanabygg.no>
2026-05-01 23:58:13 +0200

    Delete check_background_guard.py

--- check_background_guard.py
+++
@@ -1,9 +0,0 @@
-
-from background_guard import print_market_guard_summary, market_guard_summary
-from stocks import get_sp500_tickers
-
-print_market_guard_summary()
-
-sample = get_sp500_tickers(5)
-print("Sample:", sample)
-print("Summary:", market_guard_summary(sample))
