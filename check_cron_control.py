90f0e9f37fe8b2744c64e4232bc01d9092a0bd21
aksje-app <dag.borch@tanabygg.no>
2026-05-01 20:51:54 +0200

    Delete check_cron_control.py

--- check_cron_control.py
+++
@@ -1,9 +0,0 @@
-
-from cron_control import cron_status_text, should_run_background_scan
-
-print("Cron control status:")
-print(cron_status_text())
-
-allowed, reason = should_run_background_scan()
-print("allowed:", allowed)
-print("reason:", reason)
