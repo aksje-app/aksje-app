ce02e67db6d8ac108d23d35a2f1dd798021ebcc4
aksje-app <dag.borch@tanabygg.no>
2026-05-10 16:21:36 +0200

    Add files via upload

--- services/service_registry.py
+++ services/service_registry.py
@@ -27,3 +27,9 @@ _registry = ServiceRegistry()
 
 def get_service_registry() -> ServiceRegistry:
     return _registry
+
+def get_service_registry():
+    return _registry
+
+def build_service_registry():
+    return get_service_registry()
